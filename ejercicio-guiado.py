55c66ced8c8d8bb20bd512ed830e16c6ef653341
Ambar Zambrano Venegas <ambaar.venegas@gmail.com>
2024-07-19 21:56:57 -0400

    subiendo archivos

---
+++ ejercicio-guiado.py
@@ -0,0 +1,23 @@
+# Mi nombre es Bill, tengo 52 años y me desempeño como CEO en Microsoft. Soy Creativo, Apasionado y Visionario. Mi pasatiempo es jugar Golf y me gustaría poder jubilarme pronto.
+
+# variable 
+nombre = 'Bill'
+edad = '52'
+ocupacion = 'CEO'
+lugar = 'Microsoft'
+caracteristica_1 = 'Soy Creativo'
+caracteristica_2 = 'Apasionado'
+caracteristica_3 = 'Visionario'
+pasatiempo = 'Jugar golf'
+hacer = 'jubilarme pronto'
+
+#codigo 
+nombre = input('Ingrese su Nombre: ')
+edad = input('Ingrese Edad: ')
+ocupacion = input('Ingrese Ocupación: ')
+lugar = input('En dónde?: ')
+caracteristica_1 = input('Ingrese 3 características:\n1. > ')
+caracteristica_2 = input('2. > ')
+caracteristica_3 = input('3. > ')
+pasatiempo = input('Cuál es tu pasatiempo: ')
+hacer = input('¿Que quieres hacer? ')
